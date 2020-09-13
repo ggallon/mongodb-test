0da848167c928f2dc393b2a80dbef3df49d34d49
Gwenaël Gallon <gwenael.gallon@mac.com>
2020-09-13 23:21:10 +0200

    Add tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: 'all',
+  purge: {
+    mode: 'all',
+    content: [
+      './components/**/*.{js,ts,jsx,tsx}',
+      './pages/**/*.{js,ts,jsx,tsx}'
+    ],
+    preserveHtmlElements: true,
+    options: {
+      keyframes: true,
+    },
+  },
+  theme: {},
+  variants: {},
+  plugins: [
+    require('@tailwindcss/ui'),
+  ],
+}
