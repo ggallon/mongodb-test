60eef9332e80532ac6b19a82214c86d74ec3c0d6
Gwenaël Gallon <gwenael.gallon@mac.com>
2020-09-13 14:34:25 +0200

    Update next-auth config

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,7 +8,8 @@ const options = {
   providers: [
     Providers.Google({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code'
     }),
   ],
   // Database optional. MySQL, Maria DB, Postgres and MongoDB are supported.
